4bd56e6e91c3d337a8bbea48f208eb08090c5cb0
LeonardoFarinelli <leofarinelli@hotmail.com>
2025-06-03 12:43:32 -0300

    Create userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+exports.createUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = new User({ name, email });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Erro ao criar usuário' });
+  }
+};
